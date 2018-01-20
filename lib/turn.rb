ee6d14f368a14d2e52e0033520e10e6f82ccaf15
Michelle B <mjblevins7@gmail.com>
2018-01-20 01:19:39 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,42 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+ if (!position_taken?(board, index) && index.between?(0,8))
+   return true
+ else
+   return false
+ end
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return true
+  else
+    return true
+  end
+end
+
+def input_to_index(x)
+  x.to_i - 1
+end
+
+def move(board, position, player = "X")
+  def update_array_at_with(array, index, value)
+    array[index] = value
+  end
+  update_array_at_with(board, position, player)
+  display_board(board)
+end
+
+
+
+
+
